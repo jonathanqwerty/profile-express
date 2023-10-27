1b1f0d053eacdd891c1c44d9c2cc2e710f1fa5b3
jonathanqwerty <nathanaeljonathan09@gmail.com>
2023-10-27 16:20:55 +0700

    pertamax

---
+++ prisma/migrations/20231027085855_init/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "profiles" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "phone" TEXT,
+    "images" TEXT NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "profiles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "profiles_user_id_key" ON "profiles"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+const express = require("express"),
+  router = express.Router(),
+  controller = require("../controllers/authController"),
+  validate = require("../midellware/validate"),
+  schema = require("../validatorSchema/authValidatorSchema"),
+  checkToken = require("../midellware/checkToken"),
+  multer = require("../midellware/multer");
+
+router.post(
+  "/register",
+  multer.image.single("image"),
+  validate(schema.registerValidator),
+  controller.register
+);
+router.post("/login", validate(schema.loginValidator), controller.login);
+router.get("/profile", checkToken, controller.profile);
+router.post(
+  "/change-password",
+  checkToken,
+  validate(schema.changePasswordValidator),
+  controller.changePassword
+);
+
+module.exports = router;
